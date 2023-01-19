e6aaf102fd7712b5fab6fe1b4c53b6a07798dd88
Sergio Marentes <sergiomarentes705@gmail.com>
2023-01-19 10:10:52 -0500

    member one solution

--- src/app.js
+++ src/app.js
@@ -7,6 +7,16 @@ const premiumClients = require("./premium-clients");
 app.use("/medium-clients", mediumClients);
 app.use("/premium-clients", premiumClients);
 
+app.use("/category/:category", function (req, res, next) {
+  const category = req.params.category;
+  if (category == "a" || category == "b" || category == "c") {
+    res.redirect("/premium-clients");
+  } else {
+    res.redirect("/medium-clients");
+  }
+  next();
+});
+
 app.get("/", function (req, res) {
   res.send("Bienvenido a la api de ADA Cars");
 });
