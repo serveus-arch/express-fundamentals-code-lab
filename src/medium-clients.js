3c1ab09725f3c13d0541f6cb43d73f93a8c47e00
Sergio Marentes <sergiomarentes705@gmail.com>
2023-01-19 08:07:17 -0500

    initial config

--- src/medium-clients.js
+++ src/medium-clients.js
@@ -5,9 +5,4 @@ router.get("/", function (req, res) {
   res.send("Ruta clientes medium");
 });
 
-router.get("/list/:limit", function (req, res) {
-  const limit = req.params.limit;
-  res.send(`Listados ${limit} autos`);
-});
-
 module.exports = router;
--- src/premium-clients.js
+++ src/premium-clients.js
@@ -5,9 +5,4 @@ router.get("/", function (req, res) {
   res.send("Ruta clientes premium");
 });
 
-router.get("/list/:order", function (req, res) {
-  const order = req.params.order;
-  res.send(`Listados en orden ${order}`);
-});
-
 module.exports = router;
