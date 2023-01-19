c653a5a48d8b8ab2362312da30f144ece8a2cbb7
Sergio Marentes <sergiomarentes705@gmail.com>
2023-01-19 10:44:49 -0500

    second member solution

--- src/premium-clients.js
+++ src/premium-clients.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+function methods(req, res, next) {
+  res.header("url", req.originalUrl);
+  next();
+}
+
+router.use(methods);
+
 router.get("/", function (req, res) {
   res.send("Ruta clientes premium");
 });
