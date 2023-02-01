968370ddbbea985ca4e863f066a2705446eb13f0
Sergio Marentes <sergiomarentes705@gmail.com>
2023-02-01 13:48:46 -0500

    initial config app file

--- src/app.js
+++ src/app.js
@@ -1,21 +1,11 @@
-const express = require("express");
 const app = express();
 
-const mediumClients = require("./medium-clients");
-const premiumClients = require("./premium-clients");
+const users = [
+  { email: "admin@example.com", name: "admin", rol: "admin" },
+  { email: "user@example.com", name: "user", rol: "user" },
+];
 
-app.use("/medium-clients", mediumClients);
-app.use("/premium-clients", premiumClients);
-
-app.use("/category/:category", function (req, res, next) {
-  const category = req.params.category;
-  if (category == "a" || category == "b" || category == "c") {
-    res.redirect("/premium-clients");
-  } else {
-    res.redirect("/medium-clients");
-  }
-  next();
-});
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("Bienvenido a la api de ADA Cars");
