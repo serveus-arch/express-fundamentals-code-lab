fe60662f91bf1ec70e88d6d15e87a2c1ce570ff7
Sergio Marentes <sergiomarentes705@gmail.com>
2023-02-08 12:24:46 -0500

    Initial files configuration

--- src/medium-clients.js
+++ src/medium-clients.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-function methods(req, res, next) {
-  const method = req.method;
-  if (method === "GET") {
-    next();
-  } else {
-    res.status(405).send("Invalid http request method");
-  }
-}
-
-router.use(methods);
+const mediumClients = [
+  { nombre: "Maria", age: "23" },
+  { nombre: "Ana", age: "22" },
+  { nombre: "Ricardo", age: "22" },
+  { nombre: "Camila", age: "21" },
+  { nombre: "Angel", age: "24" },
+  { nombre: "Jeison", age: "27" },
+  { nombre: "Armando", age: "28" },
+];
 
 router.get("/", function (req, res) {
-  res.send("Ruta clientes medium");
+  const data = req.body;
+  mediumClients.push(data);
+  res.status(201).json({ createdClient: data });
+});
+
+router.post("/", function (req, res) {
+  res.json({ meetings: meetings });
+});
+
+router.put("/:id", function (req, res) {
+  res.status(200).json({ deletedClient: data });
+});
+
+router.delete("/:id", function (req, res) {
+  const id = req.params.id;
+  // Get the data from the request
+  mediumClients[id] = data;
+  res.status(200).json({ updatedClient: mediumClients[id] });
 });
 
 module.exports = router;
--- src/premium-clients.js
+++ src/premium-clients.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-function setHeader(req, res, next) {
-  res.header("url", req.originalUrl);
-  next();
-}
-
-router.use(setHeader);
+const premiumClients = [
+  { nombre: "Roberto", age: "23" },
+  { nombre: "Pablo", age: "22" },
+  { nombre: "Laura", age: "22" },
+  { nombre: "Sofia", age: "21" },
+  { nombre: "Angelica", age: "24" },
+  { nombre: "Estiven", age: "27" },
+  { nombre: "Armando", age: "28" },
+];
 
 router.get("/", function (req, res) {
-  res.send("Ruta clientes premium");
+  const data = req.body;
+  premiumClients.push(data);
+  res.status(201).json({ createdClient: data });
+});
+
+router.post("/", function (req, res) {
+  res.json({ meetings: meetings });
+});
+
+router.put("/:id", function (req, res) {
+  res.status(200).json({ deletedUser: data });
+});
+
+router.delete("/:id", function (req, res) {
+  const id = req.params.id;
+  // Get the data from the request
+  premiumClients[id] = data;
+  res.status(200).json({ updatedClient: premiumClients[id] });
 });
 
 module.exports = router;
