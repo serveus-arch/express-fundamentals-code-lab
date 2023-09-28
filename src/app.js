ca3a27e9ec1951f24a674c459f1d913d7a72c82c
Gabriel Bernal <gabrielbernalp@gmail.com>
2023-09-28 21:47:48 +0200

    Update app.js
    Fix initial lab routing to avoid students to update the same file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const mediumClientsEndpoint = require("./medium-clients");
+const premiumClientsEndpoint = require("./premium-clients");
 
 app.use(express.json());
+
+app.use("/api/medium-clients", mediumClientsEndpoint);
+app.use("/api/premium-clients", premiumClientsEndpoint);
 
 app.get("/", function (req, res) {
   res.send("Bienvenido a la api de ADA Cars");
