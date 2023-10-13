4fd2b2e46f67ecdcb486a7aba9870d60796b9caa
Gabriel Bernal <gabrielbernalp@gmail.com>
2023-10-13 18:22:40 +0200

    Update app.js to export server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-module.exports = app;
+// TODO: Completa tu código aquí ⬇️
+
+// Instancia del servidor
+const server = app.listen(PORT, () => {
+  console.log(`listening on port http://localhost:${PORT}`);
+});
+
+// Exportación del servidor
+module.exports = server;
