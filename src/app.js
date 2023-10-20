91ec8e9c404fba1248a5861eca00e0b3bbd32019
Cesar <37281311+cesarju@users.noreply.github.com>
2023-10-20 16:26:21 -0400

    Update app.js comments

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // TODO: Completa tu código aquí ⬇️
 
+//Se recomienda no editar ni eliminar la instancia del servidor.
 // Instancia del servidor
 const server = app.listen(PORT, () => {
   console.log(`listening on port http://localhost:${PORT}`);
